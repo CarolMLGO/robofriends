f82f5a9cddfc75de88204c917539239fabd09001
Lei_tiger <leifu@oregonstate.edu>
2018-10-28 22:01:33 -0700

    fix error

--- src/containers/App.js
+++ src/containers/App.js
@@ -17,7 +17,6 @@ class App extends Component {
 	}
 
 	componentDidMount() {
-		console.log(this.props.store.getState())
 		fetch('https://jsonplaceholder.typicode.com/users')
 		.then(response=> {
 			return response.json();
