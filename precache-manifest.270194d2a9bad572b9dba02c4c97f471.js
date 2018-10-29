1a86973e676aa1f2e3b12da08cf596f1532a1126
Lei_tiger <leifu@oregonstate.edu>
2018-10-28 21:38:03 -0700

    Updates

---
+++ precache-manifest.270194d2a9bad572b9dba02c4c97f471.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/robofriends/static/media/SEGA.12a3db8b.woff"
+  },
+  {
+    "revision": "1446d6ec194445c3572d",
+    "url": "/robofriends/static/js/runtime~main.1446d6ec.js"
+  },
+  {
+    "revision": "3a1fc05d21bcb8c40fd5",
+    "url": "/robofriends/static/js/main.3a1fc05d.chunk.js"
+  },
+  {
+    "revision": "e745872e1108711b7eda",
+    "url": "/robofriends/static/js/1.e745872e.chunk.js"
+  },
+  {
+    "revision": "3a1fc05d21bcb8c40fd5",
+    "url": "/robofriends/static/css/main.22e5cbca.chunk.css"
+  },
+  {
+    "revision": "e745872e1108711b7eda",
+    "url": "/robofriends/static/css/1.e9d2689e.chunk.css"
+  },
+  {
+    "revision": "fc0a78f0b1ce3915dcf1a3623018c424",
+    "url": "/robofriends/index.html"
+  }
+];
